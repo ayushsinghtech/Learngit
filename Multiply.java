9e6bfaf12c7d92a7973367be6b69d442fec66137
Ayush <ayushsinghtechcs@gmail.com>
2023-09-21 09:44:48 +0530

    Multiply program added

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Multiply{
+    public static void main(String args[]){
+
+        int a=2,b=3;
+        int mul=a*b;
+
+        System.out.println("Multiply is:"+mul);
+    }
+}
