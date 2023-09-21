3a3ab9948135f1b8fa36ef8b6ed8d067e0bbbf38
Ayush <ayushsinghtechcs@gmail.com>
2023-09-21 09:36:21 +0530

    Diff program added

---
+++ Diff.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Diff{
+    public static void main(String args[]){
+
+        int a=2,b=3;
+        int diff=a+b;
+
+        System.out.println("Diff is:"+diff);
+    }
+}
